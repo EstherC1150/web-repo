864aa735d7fcd1323febd7048bfeb7e7efb66865
dayechoi <dayechoi@DESKTOP-30OVI77.mshome.net>
2023-10-26 01:37:45 +0900

    달력만들기 과제 -> 위 br 3개 해결하기

---
+++ helloWeb/src/main/webapp/todo/cal.js
@@ -0,0 +1,32 @@
+		//calendar => table
+		//makeHead()
+		function makeHead() {
+			const day = ['일', '월', '화', '수', '목', '금', '토'];
+			let dayStr = ''; 
+			dayStr += "<table border = '1'>";
+			dayStr += "<thead>";
+			dayStr += "<tr>";
+			
+			day.forEach(obj => {dayStr += "<th>" + obj + "</th>"});
+			dayStr += "</tr></thead>";
+		
+		//makeBody()
+		//function makeBody() {
+			dayStr += "<tbody><tr>";
+			for(let i=1; i<32; i++) {
+				dayStr += '<td>' + i + '</td>'
+				if(i % 7 == 0) {
+					dayStr += '<br><tr>'
+				}
+			}
+			dayStr += '</tr></tbody></table>';
+			return dayStr;
+		}
+		
+		//makeCalendar()
+		function makeCalendar() {
+			document.getElementById('show').innerHTML = makeHead();
+		}
+		makeCalendar();
+		//위 두 함수 조합하여 적절하게 써서
+		//makeCalendar();
